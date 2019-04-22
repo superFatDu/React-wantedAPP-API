aa924fbf7d90eae1ab43b39d264104c4c9d7e581
superFatDu <superduchengwu@163.com>
2019-04-23 00:08:05 +0800

    add login interface

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require('koa-router')();
 const User = require("../dbs/models/User.js");
+
 router.prefix('/user')
 
 router.get('/getUserInfo', async (ctx, next) => {
@@ -27,12 +28,35 @@ router.post('/regInfo', async (ctx, next) => {
       ctx.body = {
         code: 0, 
         msg: "注册成功",
-        loginMsg: res.user
+        regMsg: {
+          user: res.user,
+          type: res.type
+        }
       }
     } else {
       ctx.body = {
         code: 1, 
         msg: "注册失败，请重试"
+      }
+    }
+  }
+})
+
+router.post("/login", async (ctx, next) => {
+  const { user, pwd } = ctx.request.body;
+  let res = await User.findOne({user, pwd});
+  if (!res) {
+    ctx.body = {
+      code: 1,
+      msg: "用户名或者密码不正确"
+    }
+  } else {
+    ctx.body = {
+      code: 0, 
+      msg: "登录成功",
+      loginMsg: {
+        user: res.user,
+        type: res.type
       }
     }
   }
