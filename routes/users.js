0e50962ec4a84d37815cebcea3e1740beede7f2b
superFatDu <superduchengwu@163.com>
2019-04-22 20:41:05 +0800

    update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const router = require('koa-router')();
 const User = require("../dbs/models/User.js");
+// const koaCookie = require('koa-cookie');
 
-router.prefix('/users')
+// router.use(koaCookie());
+router.prefix('/user')
 
-router.get('/getUser', async (ctx, next) => {
+router.get('/getUserInfo', async (ctx, next) => {
   let res = await User.find((err, doc) => {
     if (err) {
       return err
@@ -12,11 +14,31 @@ router.get('/getUser', async (ctx, next) => {
     }
   });
   ctx.body = res;
-  next();
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+router.post('/regInfo', async (ctx, next) => {
+  const { user, pwd, type } = ctx.request.body;
+  let res = await User.findOne({user});
+  if (res) {
+    ctx.body = {
+          code: 1,
+          msg: "用户名已存在"
+        }
+  } else {
+    let res = await new User({user, pwd, type}).save();
+    if (res) {
+      ctx.body = {
+        code: 0, 
+        msg: "注册成功",
+        loginMsg: res.user
+      }
+    } else {
+      ctx.body = {
+        code: 1, 
+        msg: "注册失败，请重试"
+      }
+    }
+  }
 })
 
 module.exports = router
