72f0c77871ab971e2763dd55e9df86ca7983025f
superFatDu <superduchengwu@163.com>
2019-04-26 14:21:56 +0800

    updated

--- routes/chats.js
+++ routes/chats.js
@@ -5,8 +5,11 @@ const User = require("../dbs/models/User.js");
 router.prefix('/chat');
 
 router.post("/init", async (ctx) => {
-  const {chat_id} = ctx.request.body;
-  let res = await Chat.find({chat_id}, null, {sort: 'create_time'});
+  // const {chat_id} = ctx.request.body;
+  const user = ctx.cookies.get("userId");
+  // let res = await Chat.find({chat_id}, null, {sort: 'create_time'});
+  //let res = await Chat.find({from: user}).or({to: user})
+  let res = await Chat.find({'$or': [{from: user}, {to: user}]});
   ctx.body = {
     code: 0,
     msg: res
