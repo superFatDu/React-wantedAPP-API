36d90254b8fc931604d88055f8b6b777650715d2
superFatDu <superduchengwu@163.com>
2019-04-26 14:35:15 +0800

    rebuild the constructure.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
+   ctx.body = {
+    code: 1
+  }
 })
 
 router.get('/string', async (ctx, next) => {
